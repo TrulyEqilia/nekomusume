d4d2cb487b1fe2f25132ab03b9e1fd584af54ffe
Eqilia <trulyeqilia@gmail.com>
2024-05-16 09:12:55 +0200

    chore: Add utils/create-build-skeleton.sh
    Basically, it creates the bases for building via make.

---
+++ utils/create-build-skeleton.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+mkdir build
+mkdir build/objects
+mkdir build/objects/src
+mkdir build/objects/shared
+
+mkdir build/objects/src/lib
+mkdir build/objects/src/lib/common
+mkdir build/objects/src/lib/common/fs
+mkdir build/objects/src/lib/common/char
+
+mkdir build/objects/src/cli
